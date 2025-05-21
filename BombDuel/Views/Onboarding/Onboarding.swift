d35c56e5d8010e131ec586daa2ec28e31516665e
Deannv <afilfriend@gmail.com>
2025-05-21 11:43:45 +0800

    feat: fresh

---
+++ BombDuel/Views/Onboarding/Onboarding.swift
@@ -0,0 +1,18 @@
+//
+//  Onboarding.swift
+//  BombDuel
+//
+//  Created by Kemas Deanova on 21/05/25.
+//
+
+import SwiftUI
+
+struct Onboarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Onboarding()
+}
