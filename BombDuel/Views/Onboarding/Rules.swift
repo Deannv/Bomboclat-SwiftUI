e722511dbdf9c954f90da7fa33c3237d7893787d
Deannv <afilfriend@gmail.com>
2025-05-21 11:52:28 +0800

    feat: add onboarding views

--- BombDuel/Views/Onboarding/Rules.swift
+++ BombDuel/Views/Onboarding/Rules.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct Rules: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .top)
+        {
+            Image("onboarding1")
+                .resizable()
+                .frame(width: 270, height: 300)
+                .padding(.top, -200)
+        }
+        .ignoresSafeArea()
     }
 }
 
